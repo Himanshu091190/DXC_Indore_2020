35a31f70bd4fcc3ccd9d0ba89724bfe7ac358379
himanshu091190 <himanshu091190@hotmail.com>
2020-07-08 12:12:28 +0530

    1. Servlet Project

---
+++ servletProject/src/com/dxc/servletsprogram/FirstServlet.java
@@ -0,0 +1,29 @@
+package com.dxc.servletsprogram;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class FirstServlet
+ */
+public class FirstServlet extends HttpServlet {
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+		PrintWriter pw = response.getWriter();
+		pw.println("<html><body>");
+		pw.println("Welcome to Servlets");
+		pw.println("</body></html>");
+		pw.close();
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
---
+++ servletProject/src/com/dxc/servletsprogram/LoginServlet.java
@@ -0,0 +1,27 @@
+package com.dxc.servletsprogram;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class LoginServlet
+ */
+public class LoginServlet extends HttpServlet {
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter pw = response.getWriter();
+		pw.print("<html><body>");
+		pw.print(request.getParameter("uname")+", "+request.getParameter("pwd"));
+		pw.print("</body></html>");
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
