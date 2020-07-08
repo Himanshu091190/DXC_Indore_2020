59dee05652e46769849e019f7e30d4cc42cced7a
himanshu091190 <himanshu091190@hotmail.com>
2020-07-08 14:55:51 +0530

    1. Session Handling in Servlets Implemented

--- servletProject/src/com/dxc/servletsprogram/FirstServlet.java
+++ servletProject/src/com/dxc/servletsprogram/FirstServlet.java
@@ -4,9 +4,11 @@ import java.io.IOException;
 import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  * Servlet implementation class FirstServlet
@@ -18,10 +20,26 @@ public class FirstServlet extends HttpServlet {
 		PrintWriter pw = response.getWriter();
 		pw.println("<html><body>");
 		pw.println("Welcome to Servlets");
-		pw.println("</body></html>");
+		
+		HttpSession session = request.getSession(false);
+		String username = (String) session.getAttribute("uname");
+		String password = (String) session.getAttribute("pwd");
+		String atmpinhidden = (String) session.getAttribute("atmpin");
+		
+		pw.println("Username is "+username+"<br><br>");
+		pw.println("Password is "+password+"<br><br>");
+		pw.println("ATM PIN is "+atmpinhidden+"<br><br><br><br>");
 		
 		
-		response.sendRedirect("https://www.google.com/");
+		Cookie mCookie[] = request.getCookies();
+		if(mCookie != null) {
+			pw.println("Your username is "+ mCookie[0].getValue());
+			pw.println("Your password is "+mCookie[1].getValue());
+		}
+		else
+			pw.println("no cookie");
+			pw.println("</body></html>");
+		//response.sendRedirect("https://www.google.com/");
 		pw.close();
 	}
 
--- servletProject/src/com/dxc/servletsprogram/LoginServlet.java
+++ servletProject/src/com/dxc/servletsprogram/LoginServlet.java
@@ -5,9 +5,11 @@ import java.io.PrintWriter;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  * Servlet implementation class LoginServlet
@@ -18,9 +20,20 @@ public class LoginServlet extends HttpServlet {
 		PrintWriter pw = response.getWriter();
 		String username = request.getParameter("uname");
 		String password = request.getParameter("pwd");
+		String atmpinhidden = request.getParameter("atmpin");
+		Cookie mCookie = new Cookie("name",username);
+		Cookie mCookie1 = new Cookie("pwd",password);
+		response.addCookie(mCookie);
+		response.addCookie(mCookie1);
+		
+		HttpSession session = request.getSession();
+		session.setAttribute("uname", username);
+		session.setAttribute("pwd", password);
+		session.setAttribute("atmpin", atmpinhidden);
+		
 		if(username.equalsIgnoreCase("himanshu") && password.equalsIgnoreCase("sharma")) {
 			pw.print("<html><body>");
-			pw.print(username+", "+password);
+			pw.print(username+", "+password+", hidden ATM pin is "+atmpinhidden);
 			pw.print("</body></html>");	
 		}
 		else {
