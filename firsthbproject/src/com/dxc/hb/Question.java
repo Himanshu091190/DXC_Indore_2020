b2ca5dfa1c5b7db0dad1c5345c3bb34ed0461480
himanshu091190 <himanshu091190@hotmail.com>
2020-07-11 12:41:35 +0530

    1. Collections Implementation in Hibernate with Fetching Data From DB

---
+++ firsthbproject/src/com/dxc/hb/Question.java
@@ -0,0 +1,54 @@
+package com.dxc.hb;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+public class Question {
+
+	int id;
+	String questionName;
+	List<String> answers;
+	Set<String> mSetAnswers;
+	Map<String,String> mMapAnswers;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getQuestionName() {
+		return questionName;
+	}
+
+	public void setQuestionName(String questionName) {
+		this.questionName = questionName;
+	}
+
+	public List<String> getAnswers() {
+		return answers;
+	}
+
+	public void setAnswers(List<String> answers) {
+		this.answers = answers;
+	}
+
+	public Set<String> getmSetAnswers() {
+		return mSetAnswers;
+	}
+
+	public void setmSetAnswers(Set<String> mSetAnswers) {
+		this.mSetAnswers = mSetAnswers;
+	}
+
+	public Map<String, String> getmMapAnswers() {
+		return mMapAnswers;
+	}
+
+	public void setmMapAnswers(Map<String, String> mMapAnswers) {
+		this.mMapAnswers = mMapAnswers;
+	}
+}
