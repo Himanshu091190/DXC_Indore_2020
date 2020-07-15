54819f9f4eab04876e3aa1f1b1db2ac3bc5fe027
himanshu091190 <himanshu091190@hotmail.com>
2020-07-15 14:53:20 +0530

    1. Spring Boot First Application Created

---
+++ SpringbootDxc/src/main/java/com/dxc/main/SpringbootDxcApplication.java
@@ -0,0 +1,14 @@
+package com.dxc.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDxcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootDxcApplication.class, args);
+		System.out.println("Started Successfully");
+	}
+
+}
---
+++ SpringbootDxc/src/test/java/com/dxc/main/SpringbootDxcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dxc.main;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootDxcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
